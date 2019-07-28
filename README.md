b2555b43af6c1bd8f0411edd5ba5ef32ac2170f0
David <david@mailaender.it>
2019-07-28 15:53:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# telegram-notifier
+
+Module for the Z-Way Home Automation server that forwards notifications to a Telegram chat over the Bot API. Messages can be customized by device/status through the app settings, no separate device is created.
---
+++ index.js
@@ -0,0 +1,75 @@
+/**
+ * Telegram notifier for Z-Way home automation server.
+ *
+ * Sends notifications to Telegram chats with your bot. Notifications can be
+ * customized by device and/or value.
+ *
+ * Copyright 2019 David Mailänder 
+ * Released under the GNU GPLv3, see https://www.gnu.org/licenses/gpl.txt.
+ * 
+ * https://gitlab.com/z-way-modules/telegram-notifier
+ **/
+
+// ----------------------------------------------------------------------------
+// --- Class definition, inheritance and setup
+// ----------------------------------------------------------------------------
+function TelegramNotifier (id, controller) {
+	TelegramNotifier.super_.call(this, id, controller);
+	this.url = null;
+};
+
+inherits(TelegramNotifier, AutomationModule);
+_module = TelegramNotifier;
+
+// ----------------------------------------------------------------------------
+// --- Module instance initialized
+// ----------------------------------------------------------------------------
+TelegramNotifier.prototype.init = function (config) {
+	TelegramNotifier.super_.prototype.init.call(this, config);
+
+	var self = this;	
+	this.url = "https://api.telegram.org/bot" + config.token + "/sendMessage";
+
+	this.notificationHandler = function (notification) {
+		// react to device notifications only
+		if (notification.level.indexOf('device') < 0) {
+			return;
+		}
+		// look for device/value specific text
+		var text = config.devices.filter(function(x) {
+			return x.device_id == notification.source && (x.value == notification.message.l || !x.value);
+		});
+		// not found, compose default message "<device>: <value>"
+		if (typeof text[0] === 'undefined') {
+			text = notification.message.dev + ": " + notification.message.l;
+		// otherwise use supplied message replacing $DEVICE and $VALUE
+		} else {
+			text = text[0].message.replace('$DEVICE', notification.message.dev).replace('$VALUE', notification.message.l);
+		}
+		// send Telegram API notification
+		http.request({
+			method: "POST",
+			url: self.url,
+			async: true,
+			data: {
+				chat_id: config.chat_id,
+				text: text
+			},
+			complete: function(response) {
+				// log success and/or error messages
+				if (response.status == 200 && config.log_level == 2) {
+					self.addNotification('info', 'Sent Telegram Notification: ' + JSON.stringify(notification), 'module');
+				} else if (response.status > 200 && config.log_level > 0) {
+					self.addNotification('error', 'Error sending Telegram notification: ' + (typeof response !== 'string'? JSON.stringify(response) : response), 'module');
+				}				
+			}
+		});
+	};
+	// register notification handler
+	self.controller.on('notifications.push', self.notificationHandler);
+};
+
+TelegramNotifier.prototype.stop = function () {
+	TelegramNotifier.super_.prototype.stop.call(this);
+	this.controller.off('notifications.push', this.notificationHandler);	
+};
