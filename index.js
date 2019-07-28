27cf5715ca85d4e32643d8f1c3fdbdcd2e14c0ca
David <david@mailaender.it>
2019-07-28 19:54:20 +0200

    Updated icon and configuration form

--- index.js
+++ index.js
@@ -13,7 +13,7 @@
 // ----------------------------------------------------------------------------
 // --- Class definition, inheritance and setup
 // ----------------------------------------------------------------------------
-function TelegramNotifier (id, controller) {
+function TelegramNotifier(id, controller) {
 	TelegramNotifier.super_.call(this, id, controller);
 	this.url = null;
 };
@@ -26,23 +26,29 @@ _module = TelegramNotifier;
 // ----------------------------------------------------------------------------
 TelegramNotifier.prototype.init = function (config) {
 	TelegramNotifier.super_.prototype.init.call(this, config);
-
-	var self = this;	
+	var self = this;
 	this.url = "https://api.telegram.org/bot" + config.token + "/sendMessage";
-
+	// sort devices by id and value to ensure complete matches are found first
+	config.devices.sort(function (x, y) {
+		if (x.device_id && !y.device_id || x.value && !y.value) {
+			return -1;
+		}
+		return 1;
+	})
+	// notification handler
 	this.notificationHandler = function (notification) {
 		// react to device notifications only
 		if (notification.level.indexOf('device') < 0) {
 			return;
 		}
-		// look for device/value specific text
-		var text = config.devices.filter(function(x) {
-			return x.device_id == notification.source && (x.value == notification.message.l || !x.value);
+		// look for device/value specific text: device and value, device without value, default = no device and no value		
+		var text = config.devices.filter(function (x) {
+			return (x.device_id == notification.source && x.value == notification.message.l) || (x.device_id == notification.source && !x.value) || (!x.device_id && !x.value);
 		});
 		// not found, compose default message "<device>: <value>"
 		if (typeof text[0] === 'undefined') {
 			text = notification.message.dev + ": " + notification.message.l;
-		// otherwise use supplied message replacing $DEVICE and $VALUE
+			// otherwise use supplied message replacing $DEVICE and $VALUE
 		} else {
 			text = text[0].message.replace('$DEVICE', notification.message.dev).replace('$VALUE', notification.message.l);
 		}
@@ -55,13 +61,13 @@ TelegramNotifier.prototype.init = function (config) {
 				chat_id: config.chat_id,
 				text: text
 			},
-			complete: function(response) {
+			complete: function (response) {
 				// log success and/or error messages
 				if (response.status == 200 && config.log_level == 2) {
 					self.addNotification('info', 'Sent Telegram Notification: ' + JSON.stringify(notification), 'module');
 				} else if (response.status > 200 && config.log_level > 0) {
-					self.addNotification('error', 'Error sending Telegram notification: ' + (typeof response !== 'string'? JSON.stringify(response) : response), 'module');
-				}				
+					self.addNotification('error', 'Error sending Telegram notification: ' + (typeof response !== 'string' ? JSON.stringify(response) : response), 'module');
+				}
 			}
 		});
 	};
@@ -71,5 +77,5 @@ TelegramNotifier.prototype.init = function (config) {
 
 TelegramNotifier.prototype.stop = function () {
 	TelegramNotifier.super_.prototype.stop.call(this);
-	this.controller.off('notifications.push', this.notificationHandler);	
+	this.controller.off('notifications.push', this.notificationHandler);
 };
