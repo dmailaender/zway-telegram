f9d3ad0c5133eddcbab572dd3d3c6a4506485427
David <david@mailaender.it>
2019-08-04 23:53:07 +0200

    Periodic notification added

--- index.js
+++ index.js
@@ -20,7 +20,9 @@
 // ----------------------------------------------------------------------------
 function TelegramNotifier(id, controller) {
 	TelegramNotifier.super_.call(this, id, controller);
-	this.url = null;
+	this.url = "";		// Telegram URL
+	this.messages = {}; // collected messages
+	this.interval = [];
 };
 
 inherits(TelegramNotifier, AutomationModule);
@@ -33,54 +35,142 @@ TelegramNotifier.prototype.init = function (config) {
 	TelegramNotifier.super_.prototype.init.call(this, config);
 	var self = this;
 	this.url = "https://api.telegram.org/bot" + config.token + "/sendMessage";
-	// sort devices by id and value to ensure complete matches are found first
-	config.devices.sort(function (x, y) {
-		if (x.device_id && !y.device_id || x.value && !y.value) {
+	// sort per-device notifications by id and value to ensure complete matches are found first
+	config.deviceNotifications.sort(function (x, y) {
+		if (x.deviceId && !y.deviceId || x.value && !y.value) {
 			return -1;
 		}
 		return 1;
 	})
-	// notification handler
+	// event handler for notifications
 	this.notificationHandler = function (notification) {
 		// react to device notifications only
 		if (notification.level.indexOf('device') < 0) {
 			return;
 		}
-		// look for device/value specific text: device and value, device without value, default = no device and no value		
-		var text = config.devices.filter(function (x) {
-			return (x.device_id == notification.source && x.value == notification.message.l) || (x.device_id == notification.source && !x.value) || (!x.device_id && !x.value);
+		// look for device/value specific text: first device and value then device without value
+		var device = self.find(config.deviceNotifications, function (x) {
+			return x.deviceId === notification.source && (x.value === notification.message.l || !x.value);
 		});
-		// not found, compose default message "<device>: <value>"
-		if (typeof text[0] === 'undefined') {
-			text = notification.message.dev + ": " + notification.message.l;
-			// otherwise use supplied message replacing $DEVICE and $VALUE
-		} else {
-			text = text[0].message.replace('$DEVICE', notification.message.dev).replace('$VALUE', notification.message.l);
-		}
-		// send Telegram API notification
-		http.request({
-			method: "POST",
-			url: self.url,
-			async: true,
-			data: {
-				chat_id: config.chat_id,
-				text: text
-			},
-			complete: function (response) {
-				// log success and/or error messages
-				if (response.status == 200 && config.log_level == 2) {
-					self.addNotification('info', 'Sent Telegram Notification: ' + JSON.stringify(notification), 'module');
-				} else if (response.status > 200 && config.log_level > 0) {
-					self.addNotification('error', 'Error sending Telegram notification: ' + (typeof response !== 'string' ? JSON.stringify(response) : response), 'module');
-				}
+		var message = "";
+		// device not configured, but everything forwarded
+		if (device === false && config.forwardAllNotifications) {
+			message = self.compose(config.defaultMessage, notification);
+			if (config.collectDefaultMessages) {
+				self.collect(notification, message);
+				return;
 			}
-		});
+			// device found and not ignored
+		} else if (device.deviceId !== "" && device.options !== 'ignore') {
+			message = self.compose(device.message ? device.message : config.defaultMessage, notification);
+			if (device.options == 'collect') {
+				self.collect(notification, message);
+				return;
+			}
+		}
+		self.send(message);
 	};
+	// schedule periodic transfer for collected messages
+	if (config.collectDefaultMessages || config.deviceNotifications.some(function (x) { return x.options === 'collect' })) {
+		// parse time string into timestamp array
+		var now = new Date(Date.now());
+		this.interval = config.notificationInterval.split(',')
+			.map(function (x) {
+				var time = x.trim().split(':');
+				return new Date(now.getFullYear(), now.getMonth(), now.getDate(), parseInt(time[0]), parseInt(time[1])).getTime();
+			})
+			.sort(function (x, y) {
+				return x > y;
+			});
+		this.collector = function () {
+			self.send(JSON.stringify(self.messages));
+			self.length = 0;
+			clearTimeout(self.collector);
+			self.schedule();
+		}
+		this.schedule();
+	}
 	// register notification handler
-	self.controller.on('notifications.push', self.notificationHandler);
+	self.controller.on('notifications.push', this.notificationHandler);
 };
 
 TelegramNotifier.prototype.stop = function () {
 	TelegramNotifier.super_.prototype.stop.call(this);
 	this.controller.off('notifications.push', this.notificationHandler);
 };
+
+// ----------------------------------------------------------------------------
+// --- Module functions
+// ----------------------------------------------------------------------------
+// for a comparison of prototype vs objects methods see:
+// https://veerasundar.com/blog/2014/02/javascript-prototype-methods-vs-object-methods/
+// ----------------------------------------------------------------------------
+TelegramNotifier.prototype.oneDay = 24 * 60 * 60 * 1000;
+
+TelegramNotifier.prototype.compose = function (message, notification) {
+	return message
+		.replace('$TIME', notification.timestamp)
+		.replace('$DEVICE', notification.message.dev)
+		.replace('$VALUE', notification.message.l);
+}
+
+TelegramNotifier.prototype.collect = function (notification, message) {
+	if (typeof this.messages[notification.message.dev] === 'undefined') {
+		this.messages[notification.message.dev] = [];
+	}
+	this.messages[notification.message.dev].push({
+		'time': notification.timestamp,
+		'message': message
+	});
+}
+
+TelegramNotifier.prototype.send = function (message) {
+	var self = this;
+	http.request({
+		method: "POST",
+		url: self.url,
+		async: true,
+		data: {
+			chat_id: config.chatId,
+			text: message
+		},
+		complete: function (response) {
+			// log success and/or error messages
+			if (response.status == 200 && config.logLevel == 2) {
+				self.addNotification('info', 'Sent Telegram Notification: ' + JSON.stringify(notification), 'module');
+			} else if (response.status > 200 && config.logLevel > 0) {
+				self.addNotification('error', 'Error sending Telegram notification: ' + (typeof response !== 'string' ? JSON.stringify(response) : response), 'module');
+			}
+		}
+	});
+}
+
+TelegramNotifier.prototype.schedule = function () {
+	var now = Date.now();
+	// try to find next or previous run time
+	var next = this.indexOf(this.interval, function (x) { return x > now });
+	if (next === -1) {
+		next = this.indexOf(this.interval, function (x) { return x < now });
+		this.interval[next] += this.oneDay;
+	}
+	// console.debug("[telegram] notification scheduled for " + new Date(this.interval[next]));
+	setTimeout(this.collector, this.interval[next] - now);
+}
+
+TelegramNotifier.prototype.indexOf = function (array, func) {
+	for (var i = 0; i < array.length; i++) {
+		if (func(array[i]) === true) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+TelegramNotifier.prototype.find = function (array, func) {
+	for (var i = 0; i < array.length; i++) {
+		if (func(array[i]) === true) {
+			return array[i];
+		}
+	}
+	return false;
+}
